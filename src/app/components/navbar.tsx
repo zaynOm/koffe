f162545154ef85d301af3ff101ebd1b1d76ccadf
zaynom <ouaziz0626@gmail.com>
2024-10-24 22:40:13 +0100

    Add navbar

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header>
+      <div className="container flex justify-between items-center h-16">
+        <div>
+          <Link href="/" className="font-display text-3xl font-bold">
+            koffe
+          </Link>
+        </div>
+        <menu className="space-x-4 font-medium hidden sm:block">
+          <Link href="/Menu">Menu</Link>
+          <Link href="/About">About</Link>
+          <Link href="/Contact">Contact</Link>
+        </menu>
+        <button className="bg-gradient-to-tr from-button-bg from-40% font-medium py-2 px-6 text-button-fg rounded-full">
+          Order Now
+        </button>
+      </div>
+    </header>
+  );
+}
