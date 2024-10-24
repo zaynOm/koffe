17be780d52f945c702113905d161025fad453a4d
zaynom <ouaziz0626@gmail.com>
2024-10-24 16:24:50 +0100

    Add custom styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 const config: Config = {
   content: [
@@ -7,11 +8,25 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    fontFamily: {
+      sans: ["var(--font-geist-sans)"],
+      mono: ["var(--font-geist-mono)"],
+      display: ["var(--font-bricolage)"],
+    },
+    colors: {
+      transparent: "transparent",
+      background: "hsl(var(--background))",
+      foreground: "hsl(var(--foreground))",
+      primary: "hsl(var(--primary))",
+      button: {
+        bg: "hsl(var(--button-bg))",
+        fg: "hsl(var(--button-fg))",
       },
+      gray: colors.slate,
     },
   },
   plugins: [],
