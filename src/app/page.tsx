a3b1252d16c33e799233710e689be119da54c199
zaynom <ouaziz0626@gmail.com>
2024-10-24 16:24:10 +0100

    Add sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import Hero from "./components/hero";
+import Listings from "./components/listings";
+import Search from "./components/search";
 
 export default function Home() {
   return (
+    <main>
+      <div className="container">
+        <Hero />
+        <Search />
+        <Listings />
+      </div>
+    </main>
   );
 }
