a4a0b345abac40fc1e1398508f00e58679a71fe5
zaynom <ouaziz0626@gmail.com>
2024-10-25 12:51:37 +0100

    Fix flag miss spell "--turbo" -> "--turbopack"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
+"use client";
 import CallToAction from "./components/call-to-action";
 import Hero from "./components/hero";
 import Listings from "./components/listings";
 import Reviews from "./components/reviews";
-import Search from "./components/search";
 
 export default function Home() {
   return (
     <main>
       <div className="container">
         <Hero />
-        <Search />
         <Listings />
         <Reviews />
         <CallToAction />
