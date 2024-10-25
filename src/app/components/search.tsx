cbf31159d411f02407855274e603f5e0de3ab807
zaynom <ouaziz0626@gmail.com>
2024-10-25 15:08:15 +0100

    Target search input for smooth scrolling

--- src/app/components/search.tsx
+++ src/app/components/search.tsx
@@ -1,4 +1,5 @@
 import { SearchIcon } from "lucide-react";
+import { useRef } from "react";
 
 type SearchProps = {
   searchQuery: string;
@@ -6,8 +7,9 @@ type SearchProps = {
 };
 
 export function Search({ searchQuery, setSearchQuery }: SearchProps) {
+  const ref = useRef<HTMLDivElement>(null);
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center pt-4" ref={ref}>
       <div className="border-2 rounded-full w-[400px] flex items-center px-4 gap-3">
         <SearchIcon className="text-gray-400" />
         <input
@@ -15,7 +17,13 @@ export function Search({ searchQuery, setSearchQuery }: SearchProps) {
           placeholder="What do you want to drink?"
           className="h-14 flex-1 bg-transparent focus:outline-none text-lg "
           value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
+          onChange={(e) => {
+            ref.current?.scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+            });
+            setSearchQuery(e.target.value);
+          }}
         />
       </div>
     </div>
