ecb89e08ee2db706a1010f39a07db908253eabb7
zaynom <ouaziz0626@gmail.com>
2024-10-25 15:06:45 +0100

    Update hero section

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -2,9 +2,9 @@ import { BadgePercent } from "lucide-react";
 
 export default function Hero() {
   return (
-    <main className="flex justify-center items-center">
-      <div className="py-24 flex flex-col items-center text-center gap-6 max-w-4xl">
-        <h1 className="font-bold text-6xl font-display">
+    <main className="flex justify-center items-center min-h-screen">
+      <div className="pb-24 flex flex-col items-center text-center gap-10 max-w-5xl">
+        <h1 className="font-bold text-6xl sm:text-7xl font-display">
           We deliver nothing but the finest coffee experience
         </h1>
         <button className="inline-flex items-center justify-center gap-2 bg-gradient-to-tr from-button-bg from-40% font-medium py-2 px-6 text-button-fg rounded-full">
